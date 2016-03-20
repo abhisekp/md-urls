d579da2da26588b1b3ac5aba2e519ba83d9e7939
Abhisek Pattnaik <abhisekp@engineer.com>
2016-03-20 10:36:38 +0530

    Add cli sample code

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,39 @@
 #!/usr/bin/env babel-node
 
-import * as commander from 'commander';
+'use strict';
+
+import * as program from 'commander';
+import promisify from 'promisify-lite';
+import * as fs from 'fs';
+import * as path from 'path';
+import log4js from 'log4js';
+import json5 from 'json5';
+
+fs = promisify(fs);
+
+const cliLog = log4js.getLogger('CLI LOGGER');
+
+const getPackageFile = () => {
+  const PACKAGE_PATH = path.resolve('./package.json');
+  cliLog.debug(`Package Path: ${PACKAGE_PATH}`);
+
+  let packageFileContent = null;
+
+  return (async () => {
+    if (!packageFileContent) {
+      packageFileContent = await fs.readFileAsync(PACKAGE_PATH, 'utf-8');
+    }
+
+    cliLog.info(`Typeof fileContent: ${typeof packageFileContent}`); // <--- string
+    cliLog.debug(`Package file content:\n${packageFileContent}`);
+
+    return packageFileContent;
+  })();
+};
+
+const getVersion = async () => {
+  const VERSION = json5.parse(await getPackageFile()).version;
+  cliLog.debug('VERSION: ', VERSION);
+};
+
+getVersion();
