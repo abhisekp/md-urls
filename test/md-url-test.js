4a1ca633b1cc3dd85e0debbd06e43d154fae7f63
Abhisek Pattnaik <abhisekp@engineer.com>
2016-03-26 00:54:51 +0530

    Add test boilerplate

--- test/md-url-test.js
+++ test/md-url-test.js
@@ -1,24 +1,32 @@
 import test from 'tape';
 import tapNotify from 'tap-notify';
+import tapDiff from 'tap-diff';
 import promisify from 'tape-promise';
 
 const testp = promisify(test);
 
-// test.createStream().pipe(tapNotify());
+testp.createStream()
+     .pipe(tapNotify())
+     .pipe(tapDiff())
+     .pipe(process.stdout);
+
+const delay = (time = 5000) => new Promise((resolve, reject) => {
+  // setTimeout(() => resolve('Success is at your feet'), time);
+  setTimeout(() => reject(Error('BIG FAIL!!!')), time + 2000);
+});
+
+testp('Hello', async (t) => {
+  try {
+    t.pass(await delay());
+  } catch (e) {
+    t.error(e, e.message);
+  } finally {
+    t.end();
+  }
+});
 
 test('Test Successful', (t) => {
   t.pass('Successful Comrade');
   t.equal('oK', 'oK', `'oK' should be equal to 'oK'`);
   t.end();
-});
-
-const delay = (time = 5000) => new Promise((resolve, reject) => {
-  setTimeout(resolve, time);
-  setTimeout(reject, time + 2000);
-});
-
-testp(async (t) => {
-    await delay();
-    t.pass('OK');
-    t.end;
 });
